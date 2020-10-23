dd1eaaff867b288f1dd49e5e322a391fa93e6b0a
Gomathi <gomathirengavilas@gmail.com>
2020-10-23 22:12:24 +0530

    (page)

---
+++ Assignments/OCT 23/Pagination/script.js
@@ -0,0 +1,719 @@
+//data to be displayed
+var data = [
+    {
+        "id": "1",
+        "name": "Lura Senger",
+        "email": "Xander_Collier@yahoo.com"
+    },
+    {
+        "id": "2",
+        "name": "Wilburn Weber",
+        "email": "Bennett_Kreiger11@yahoo.com"
+    },
+    {
+        "id": "3",
+        "name": "Tyrique Hahn",
+        "email": "Juston.Altenwerth@yahoo.com"
+    },
+    {
+        "id": "4",
+        "name": "Lenny Bailey",
+        "email": "Guiseppe_Hegmann@yahoo.com"
+    },
+    {
+        "id": "5",
+        "name": "Vladimir Keeling",
+        "email": "Louisa_Walsh18@hotmail.com"
+    },
+    {
+        "id": "6",
+        "name": "Kellie Crona",
+        "email": "Chandler_Abernathy@yahoo.com"
+    },
+    {
+        "id": "7",
+        "name": "Carolina White",
+        "email": "Royal50@hotmail.com"
+    },
+    {
+        "id": "8",
+        "name": "Alfredo Conn",
+        "email": "Clarabelle34@hotmail.com"
+    },
+    {
+        "id": "9",
+        "name": "Stan Shanahan",
+        "email": "Lamar.McClure@hotmail.com"
+    },
+    {
+        "id": "10",
+        "name": "Marvin Fay",
+        "email": "Osbaldo58@hotmail.com"
+    },
+    {
+        "id": "11",
+        "name": "Torrance Rau",
+        "email": "Orin45@gmail.com"
+    },
+    {
+        "id": "12",
+        "name": "Harold Gutmann MD",
+        "email": "Alyce.Stracke37@yahoo.com"
+    },
+    {
+        "id": "13",
+        "name": "Taryn Torphy",
+        "email": "Dean_Breitenberg71@hotmail.com"
+    },
+    {
+        "id": "14",
+        "name": "Bryana Lang",
+        "email": "Tatum.Ullrich@yahoo.com"
+    },
+    {
+        "id": "15",
+        "name": "Nyasia Green DDS",
+        "email": "Dino83@gmail.com"
+    },
+    {
+        "id": "16",
+        "name": "Nasir Gerhold",
+        "email": "Lilian_Bashirian8@hotmail.com"
+    },
+    {
+        "id": "17",
+        "name": "Raymundo Ritchie PhD",
+        "email": "Antwan.Schoen15@yahoo.com"
+    },
+    {
+        "id": "18",
+        "name": "Delmer Marvin",
+        "email": "Kiera62@yahoo.com"
+    },
+    {
+        "id": "19",
+        "name": "Rachel Wilkinson",
+        "email": "Foster_Conroy@gmail.com"
+    },
+    {
+        "id": "20",
+        "name": "Gladys Howell",
+        "email": "Constance.Labadie10@yahoo.com"
+    },
+    {
+        "id": "21",
+        "name": "Ciara Klocko",
+        "email": "Harvey76@yahoo.com"
+    },
+    {
+        "id": "22",
+        "name": "Quentin O'Kon",
+        "email": "Amely_Cummings2@yahoo.com"
+    },
+    {
+        "id": "23",
+        "name": "Ms. Gabriella Kunde",
+        "email": "Lorenza_Cummerata@hotmail.com"
+    },
+    {
+        "id": "24",
+        "name": "Gerald Reilly",
+        "email": "Lia_Konopelski@gmail.com"
+    },
+    {
+        "id": "25",
+        "name": "Brody Carter I",
+        "email": "Colten.Wilderman90@hotmail.com"
+    },
+    {
+        "id": "26",
+        "name": "Alanis Klocko",
+        "email": "Johathan.Champlin69@gmail.com"
+    },
+    {
+        "id": "27",
+        "name": "Caroline Miller",
+        "email": "Delaney.Kozey74@gmail.com"
+    },
+    {
+        "id": "28",
+        "name": "Ms. Merritt McClure",
+        "email": "Wendy.Zieme@gmail.com"
+    },
+    {
+        "id": "29",
+        "name": "Jovani Schoen",
+        "email": "Norval_Zieme@hotmail.com"
+    },
+    {
+        "id": "30",
+        "name": "Berniece Bradtke",
+        "email": "Okey.Fisher39@hotmail.com"
+    },
+    {
+        "id": "31",
+        "name": "Hanna Gottlieb",
+        "email": "Edna62@hotmail.com"
+    },
+    {
+        "id": "32",
+        "name": "Christiana Bergnaum",
+        "email": "Donnie.Paucek22@gmail.com"
+    },
+    {
+        "id": "33",
+        "name": "Reid Stehr",
+        "email": "Hiram69@gmail.com"
+    },
+    {
+        "id": "34",
+        "name": "Evans Ward",
+        "email": "Liliane72@hotmail.com"
+    },
+    {
+        "id": "35",
+        "name": "Miss Enos Davis",
+        "email": "Kolby.Grady@hotmail.com"
+    },
+    {
+        "id": "36",
+        "name": "Mathew Lueilwitz",
+        "email": "Mariah.Hagenes@yahoo.com"
+    },
+    {
+        "id": "37",
+        "name": "Romaine Robel",
+        "email": "Sheila19@yahoo.com"
+    },
+    {
+        "id": "38",
+        "name": "Immanuel Aufderhar",
+        "email": "Aileen32@hotmail.com"
+    },
+    {
+        "id": "39",
+        "name": "Julius Reilly Jr.",
+        "email": "Santa89@yahoo.com"
+    },
+    {
+        "id": "40",
+        "name": "Linnea Borer",
+        "email": "Jarrett_Hegmann2@hotmail.com"
+    },
+    {
+        "id": "41",
+        "name": "Misael Abernathy IV",
+        "email": "Robbie42@yahoo.com"
+    },
+    {
+        "id": "42",
+        "name": "Mr. Gerda Jerde",
+        "email": "Gustave36@hotmail.com"
+    },
+    {
+        "id": "43",
+        "name": "Easton Daugherty",
+        "email": "Malcolm.Schuster11@hotmail.com"
+    },
+    {
+        "id": "44",
+        "name": "Leanna Gutkowski",
+        "email": "Chelsea9@gmail.com"
+    },
+    {
+        "id": "45",
+        "name": "Rollin Trantow",
+        "email": "Marques_Hickle26@gmail.com"
+    },
+    {
+        "id": "46",
+        "name": "Heaven Luettgen",
+        "email": "Lonnie.Greenfelder95@hotmail.com"
+    },
+    {
+        "id": "47",
+        "name": "Gilda Huel",
+        "email": "Stephania.Hintz@gmail.com"
+    },
+    {
+        "id": "48",
+        "name": "Javon Walker",
+        "email": "Lamar_Bailey39@yahoo.com"
+    },
+    {
+        "id": "49",
+        "name": "Stewart King",
+        "email": "Chasity_Jenkins82@gmail.com"
+    },
+    {
+        "id": "50",
+        "name": "Cortez Lueilwitz",
+        "email": "Lelia.Mitchell43@gmail.com"
+    },
+    {
+        "id": "51",
+        "name": "Ora Kilback",
+        "email": "Matt25@gmail.com"
+    },
+    {
+        "id": "52",
+        "name": "Lera Macejkovic",
+        "email": "Jamal.Donnelly45@hotmail.com"
+    },
+    {
+        "id": "53",
+        "name": "Asa Gottlieb",
+        "email": "Carolyne.Langosh@yahoo.com"
+    },
+    {
+        "id": "54",
+        "name": "Avery Nienow",
+        "email": "Hudson_Bernhard47@yahoo.com"
+    },
+    {
+        "id": "55",
+        "name": "Dr. Katrine Ryan",
+        "email": "Colten40@gmail.com"
+    },
+    {
+        "id": "56",
+        "name": "Florida Kulas",
+        "email": "Alejandra_Bernhard@yahoo.com"
+    },
+    {
+        "id": "57",
+        "name": "Mauricio Gerlach",
+        "email": "Vincenza_Gusikowski84@hotmail.com"
+    },
+    {
+        "id": "58",
+        "name": "Chadrick Simonis",
+        "email": "Reymundo.Doyle@gmail.com"
+    },
+    {
+        "id": "59",
+        "name": "Beau Jenkins",
+        "email": "Cletus65@hotmail.com"
+    },
+    {
+        "id": "60",
+        "name": "Camilla Cole",
+        "email": "Eda48@yahoo.com"
+    },
+    {
+        "id": "61",
+        "name": "Abagail MacGyver",
+        "email": "Jevon.Koch@hotmail.com"
+    },
+    {
+        "id": "62",
+        "name": "Ludwig Prohaska",
+        "email": "Eldon_DuBuque37@yahoo.com"
+    },
+    {
+        "id": "63",
+        "name": "Berenice Hane",
+        "email": "Lonnie3@gmail.com"
+    },
+    {
+        "id": "64",
+        "name": "Abel Durgan",
+        "email": "Marlee.Weimann42@gmail.com"
+    },
+    {
+        "id": "65",
+        "name": "Tate Kuhlman",
+        "email": "Hope31@yahoo.com"
+    },
+    {
+        "id": "66",
+        "name": "Mrs. Hermina Daniel",
+        "email": "Murphy59@yahoo.com"
+    },
+    {
+        "id": "67",
+        "name": "Marcelino Nienow",
+        "email": "Estefania_Dibbert97@hotmail.com"
+    },
+    {
+        "id": "68",
+        "name": "Raphaelle Connelly",
+        "email": "Josie.Legros70@yahoo.com"
+    },
+    {
+        "id": "69",
+        "name": "Mina Heaney",
+        "email": "Pete.Hermann79@hotmail.com"
+    },
+    {
+        "id": "70",
+        "name": "Jaida Torp",
+        "email": "Jonatan82@yahoo.com"
+    },
+    {
+        "id": "71",
+        "name": "Megane Fadel",
+        "email": "Dandre82@yahoo.com"
+    },
+    {
+        "id": "72",
+        "name": "Angelo Schimmel V",
+        "email": "Frederic54@hotmail.com"
+    },
+    {
+        "id": "73",
+        "name": "Jaren Ferry DDS",
+        "email": "Kristian.Kilback@yahoo.com"
+    },
+    {
+        "id": "74",
+        "name": "Annette O'Connell",
+        "email": "Domenica.Christiansen@gmail.com"
+    },
+    {
+        "id": "75",
+        "name": "Elenor Rohan PhD",
+        "email": "Alvera.Huel48@yahoo.com"
+    },
+    {
+        "id": "76",
+        "name": "Linnie Beier",
+        "email": "Reece_Berge@gmail.com"
+    },
+    {
+        "id": "77",
+        "name": "Maybelle Lehner",
+        "email": "Arlo.Hagenes@gmail.com"
+    },
+    {
+        "id": "78",
+        "name": "Hailie Haag",
+        "email": "Edythe.Lind59@hotmail.com"
+    },
+    {
+        "id": "79",
+        "name": "Marcelina Schuppe",
+        "email": "Kip.Cummerata77@gmail.com"
+    },
+    {
+        "id": "80",
+        "name": "Ezra Lang",
+        "email": "Garret.Swaniawski@hotmail.com"
+    },
+    {
+        "id": "81",
+        "name": "Carey Kiehn",
+        "email": "Sabrina70@gmail.com"
+    },
+    {
+        "id": "82",
+        "name": "Carole Aufderhar",
+        "email": "Efrain.Schamberger77@hotmail.com"
+    },
+    {
+        "id": "83",
+        "name": "Fanny Beatty",
+        "email": "Ewald28@yahoo.com"
+    },
+    {
+        "id": "84",
+        "name": "Mina Lubowitz",
+        "email": "Vincenzo.Herman@gmail.com"
+    },
+    {
+        "id": "85",
+        "name": "Anabelle Thiel",
+        "email": "Amely.Renner@hotmail.com"
+    },
+    {
+        "id": "86",
+        "name": "Gerry Homenick",
+        "email": "Ada_Von54@hotmail.com"
+    },
+    {
+        "id": "87",
+        "name": "Domenico Maggio",
+        "email": "Freeman.Stamm@yahoo.com"
+    },
+    {
+        "id": "88",
+        "name": "Mauricio Bradtke III",
+        "email": "Alford83@yahoo.com"
+    },
+    {
+        "id": "89",
+        "name": "Bulah Yost III",
+        "email": "Nicolas_Schimmel6@yahoo.com"
+    },
+    {
+        "id": "90",
+        "name": "Orville Dibbert DDS",
+        "email": "Bryana_Herzog@yahoo.com"
+    },
+    {
+        "id": "91",
+        "name": "Autumn Osinski",
+        "email": "Davin8@hotmail.com"
+    },
+    {
+        "id": "92",
+        "name": "Levi Gibson",
+        "email": "Deangelo69@gmail.com"
+    },
+    {
+        "id": "93",
+        "name": "Reva McKenzie",
+        "email": "Bret40@hotmail.com"
+    },
+    {
+        "id": "94",
+        "name": "Jensen Pfannerstill",
+        "email": "Dax_Marquardt@hotmail.com"
+    },
+    {
+        "id": "95",
+        "name": "Tyrese Larson MD",
+        "email": "Cecelia85@yahoo.com"
+    },
+    {
+        "id": "96",
+        "name": "Dayne Sanford",
+        "email": "Tyrique_Waters16@hotmail.com"
+    },
+    {
+        "id": "97",
+        "name": "Jeramy Steuber",
+        "email": "Catalina.Stehr24@yahoo.com"
+    },
+    {
+        "id": "98",
+        "name": "Madge Bogisich",
+        "email": "Joey.Marquardt50@gmail.com"
+    },
+    {
+        "id": "99",
+        "name": "Casimer MacGyver",
+        "email": "Julius.Bednar96@yahoo.com"
+    },
+    {
+        "id": "100",
+        "name": "Arlene Kassulke",
+        "email": "Josiane_Smith@yahoo.com"
+    }
+]
+//pagination from bootstrap
+var nav = document.createElement("nav")
+nav.setAttribute('class', 'navbar')
+var list = document.createElement("ul")
+list.setAttribute("class", "pagination")
+
+var item1 = document.createElement("li")
+item1.setAttribute('class', 'page-item')
+var link1 = document.createElement("a")
+link1.setAttribute('class', "page-link");
+link1.setAttribute("href", "#")
+link1.innerHTML = 1;
+// If the numebr is clicked it invokes the function table_creator
+link1.setAttribute("onclick", 'table_creator(0, 9)')
+
+var item2 = document.createElement("li")
+item2.setAttribute("class", "page-item")
+var link2 = document.createElement("a")
+link2.setAttribute("class", "page-link")
+link2.setAttribute("href", "#")
+link2.innerHTML = 2;
+link2.setAttribute("onclick", 'table_creator(10, 19)')
+
+
+var item3 = document.createElement("li")
+item3.setAttribute("class", "page-item")
+var link3 = document.createElement("a")
+link3.setAttribute("class", "page-link")
+link3.setAttribute("href", "#")
+link3.innerHTML = 3;
+link3.setAttribute("onclick", 'table_creator(20, 29)')
+
+
+var item4 = document.createElement("li")
+item4.setAttribute("class", "page-item")
+var link4 = document.createElement("a")
+link4.setAttribute("class", "page-link")
+link4.setAttribute("href", "#")
+link4.innerHTML = 4;
+link4.setAttribute("onclick", 'table_creator(30, 39)')
+
+
+var item5 = document.createElement("li")
+item5.setAttribute("class", "page-item")
+var link5 = document.createElement("a")
+link5.setAttribute("class", "page-link")
+link5.setAttribute("href", "#")
+link5.innerHTML = 5;
+link5.setAttribute("onclick", 'table_creator(40, 49)')
+
+
+var item6 = document.createElement("li")
+item6.setAttribute("class", "page-item")
+var link6 = document.createElement("a")
+link6.setAttribute("class", "page-link")
+link6.setAttribute("href", "#")
+link6.innerHTML = 6;
+link6.setAttribute("onclick", 'table_creator(50, 59)')
+
+
+var item7 = document.createElement("li")
+item7.setAttribute("class", "page-item")
+var link7 = document.createElement("a")
+link7.setAttribute("class", "page-link")
+link7.setAttribute("href", "#")
+link7.innerHTML = 7;
+link7.setAttribute("onclick", 'table_creator(60, 69)')
+
+
+var item8 = document.createElement("li")
+item8.setAttribute("class", "page-item")
+var link8 = document.createElement("a")
+link8.setAttribute("class", "page-link")
+link8.setAttribute("href", "#")
+link8.innerHTML = 8;
+link8.setAttribute("onclick", 'table_creator(70, 79)')
+
+
+var item9 = document.createElement("li")
+item9.setAttribute("class", "page-item")
+var link9 = document.createElement("a")
+link9.setAttribute("class", "page-link")
+link9.setAttribute("href", "#")
+link9.innerHTML = 9;
+link9.setAttribute("onclick", 'table_creator(80, 89)')
+
+
+var item10 = document.createElement("li")
+item10.setAttribute("class", "page-item")
+var link10 = document.createElement("a")
+link10.setAttribute("class", "page-link")
+link10.setAttribute("href", "#")
+link10.innerHTML = 10;
+link10.setAttribute("onclick", 'table_creator(90, 99)')
+
+// creating a table
+var table = document.createElement("table")
+table.setAttribute("class", "table")
+
+// creating thead and tbody and appending it to table
+var head_row = document.createElement("thead")
+head_row.id='head';
+var row = document.createElement("tbody")
+row.id='body';
+table.append(head_row,row);
+
+
+function table_creator(min, max) {
+    // whenever this function is called it first empties both thead and tbody and then 
+// creates head and appends it to thead 
+    document.getElementById('head').innerHTML='';
+    document.getElementById('body').innerHTML='';
+    var hrow=document.createElement("tr")
+    var head1 = document.createElement("th")
+    head1.innerHTML = "ID";
+    var head2 = document.createElement("th")
+    head2.innerHTML = "Name";
+    var head3 = document.createElement("th")
+    head3.innerHTML = "E-mail";
+    hrow.append(head1, head2, head3)
+    head_row.append(hrow)
+
+    // it runs 10 times and each time creates a row and populates with 
+    // data and then appends it with tbody.
+    for (var i = min; i <= max; i++) {
+        var b_row=document.createElement("tr")
+        var data1 = document.createElement("td")
+        data1.innerHTML = data[i].id;
+        var data2 = document.createElement("td")
+        data2.innerHTML = data[i].name;
+        var data3 = document.createElement("td")
+        data3.innerHTML = data[i].email;
+        b_row.append(data1, data2, data3)
+        row.append(b_row)
+    }
+}
+
+
+
+// appending paginaiton and table to body
+item1.append(link1)
+item2.append(link2)
+item3.append(link3)
+item4.append(link4)
+item5.append(link5)
+item6.append(link6)
+item7.append(link7)
+item8.append(link8)
+item9.append(link9)
+item10.append(link10)
+
+list.append(item1, item2, item3, item4, item5, item6, item7, item8, item9, item10)
+nav.append(list)
+document.body.append(nav, table)
+
+
+
+
+
+// var first_table = document.createElement("table")
+// first_table.setAttribute("class", "table")
+
+// var head_part = document.createElement("thead")
+
+// var body_part = document.createElement("tbody")
+
+
+// first_table.append(head_part, body_part)
+// document.body.append(first_table);
+
+// var head_row = document.createElement("tr")
+//     var head1 = document.createElement("th")
+//     head1.innerHTML = "Id";
+//     var head2 = document.createElement("th")
+//     head2.innerHTML = "Name";
+//     var head3 = document.createElement("th")
+//     head3.innerHTML = "E-mail";
+//     head_row.append(head1,head2,head3);
+//     head_part.append(head_row);
+// function table_creator(min, max) {
+
+//     var head_part = document.createElement("thead")
+//     var body_part = document.createElement("tbody")
+//     first_table.append(head_part, body_part)
+//     document.body.append(first_table);
+
+//     var head_row = document.createElement("tr")
+//     var head1 = document.createElement("th")
+//     head1.innerHTML = "Id";
+//     var head2 = document.createElement("th")
+//     head2.innerHTML = "Name";
+//     var head3 = document.createElement("th")
+//     head3.innerHTML = "E-mail";
+//     head_row.append(head1,head2,head3);
+//     head_part.append(head_row);
+
+//     for (var i = min; i <= max; i++) {
+//         var row = document.createElement("tr")
+//         var td1 = document.createElement("td")
+//         td1.innerHTML = data[i].id;
+//         var td2 = document.createElement("td")
+//         td2.innerHTML = data[i].name;
+//         var td3 = document.createElement("td")
+//         td3.innerHTML = data[i].email;
+//         // console.log(td1)
+//         // console.log(td2)
+//         // console.log(row)
+//         row.append(td1, td2, td3)
+//         console.log(row)
+//         // body_part.append(row)
+//         // console.log(body_part)
+//     }
+
+// }
+
+
